4c38322dcb5f08be59e17ac6896621a669f154bb
Javohir708 <joxauzking@gamil.com>
2025-02-23 15:40:07 +0500

    pwa qo'shildi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false,
+      },
+      manifest: {
+        name: "Car Wash",
+        short_name: "Car Wash",
+        description: "Car Wash",
+        // display: "fullscreen"
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/logo_192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 })
